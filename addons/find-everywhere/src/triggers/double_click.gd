f0792c196cb062059e93854a03e4852a816de367
MakovWait <maxkv258@gmail.com>
2023-07-31 12:23:26 +0200

    add ms_ignore double_click

--- addons/find-everywhere/src/triggers/double_click.gd
+++ addons/find-everywhere/src/triggers/double_click.gd
@@ -2,14 +2,16 @@ signal triggered
 
 var _trigger_key: Key
 var _ms_detect_input: int
+var _ms_ignore_input: int
 
 var _last_time_trigger_key_pressed: int
 var _trigger_key_was_pressed: bool = false
 
 
-func _init(trigger_key: Key, ms_detect_input: int) -> void:
+func _init(trigger_key: Key, ms_detect_input: int, ms_ignore_input) -> void:
 	self._trigger_key = trigger_key
 	self._ms_detect_input = ms_detect_input
+	self._ms_ignore_input = ms_ignore_input
 
 
 func input(event: InputEvent) -> void:
@@ -18,7 +20,8 @@ func input(event: InputEvent) -> void:
 		if not key_event.keycode == _trigger_key:
 			_trigger_key_was_pressed = false
 		if key_event.keycode == _trigger_key and key_event.is_pressed():
-			if _trigger_key_was_pressed and (Time.get_ticks_msec() - _last_time_trigger_key_pressed) < _ms_detect_input:
+			var delta_time = Time.get_ticks_msec() - _last_time_trigger_key_pressed
+			if _trigger_key_was_pressed and delta_time > _ms_ignore_input and delta_time < _ms_detect_input:
 				_trigger_key_was_pressed = false
 				triggered.emit()
 			else:
